be32fd981d71e50864a4d95d80d6c4026c1018d0
ptousd <ptousd@gmail.com>
2021-12-20 19:07:36 +0100

    First Version

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BookDetailComponent } from './components/book-detail/book-detail.component';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  {
+    path: 'newest',
+    component: HomeComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'home/:id/:name',
+    component: HomeComponent,
+  },
+  {
+    path: 'book/:id',
+    component: BookDetailComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
